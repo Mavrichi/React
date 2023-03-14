f1458bd8779ed009aa4ce319ce3ba72c1ef8342b
Mavrichi <marian.mavrichi@commergent.eu>
2023-03-14 12:04:52 +0200

    Added Accordion.js component

---
+++ portfolio/src/components/Accordion.js
@@ -0,0 +1,16 @@
+﻿import {useState} from 'react';
+
+const Accordion = () => {
+    const [expand, setExpand] = useState(false);
+    const toggleExpand = () => {
+        setExpand((prevExpand) => !prevExpand);
+        console.log({expand});
+    }
+
+    return (
+        <div className="accordion">
+            <button type={"button"} onClick={toggleExpand} className={`btn btn-primary + ${expand}`}>Test</button>
+        </div>
+    );
+};
+export default Accordion;
